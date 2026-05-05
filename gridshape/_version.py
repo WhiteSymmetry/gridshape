4c61978d3d298e204af9d7f235c38a2b0417e1a8
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-05-05 01:13:58 +0000

    Update _version.py
    Signed-off-by: Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>

--- gridshape/_version.py
+++ gridshape/_version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # _version.py
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "AGPL-3.0-or-later"
 __description__ = "gridshape: A Python module for drawing geometric shapes on a cell matrix with high‑resolution subgrid support, statistical analysis and visualisation."
 __author__ = "Mehmet Keçeci"
