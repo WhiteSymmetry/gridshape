f80dbe2603362d5209af71dcf121eae30f336d4f
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-05-05 01:14:09 +0000

    Bump version to 0.1.1
    Signed-off-by: Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>

--- gridshape/__init__.py
+++ gridshape/__init__.py
@@ -6,7 +6,7 @@ with high‑resolution subgrid support, statistical analysis and visualisation.
 Main class: GridShape
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Mehmet Keçeci"
 
 from .gridshape import GridShape
